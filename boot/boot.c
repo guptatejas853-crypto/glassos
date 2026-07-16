b07aa36520717bc2b1f205dbd9be0a4a3d140ec4
guptatejas853-crypto <guptatejas853@gmail.com>
2026-07-16 21:32:02 +0530

    Move boot source into boot folder

---
+++ boot/boot.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main(void)
+{
+    printf("Starting GlassOS Boot...\n");
+    printf("Loading Glass Kernel...\n");
+    printf("Initializing Hardware...\n");
+    printf("Welcome to GlassOS!\n");
+
+    return 0;
+}

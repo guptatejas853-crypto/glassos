a8c54f1749c2c42a1fe29e7fd0a4d2239f4e63b2
guptatejas853-crypto <guptatejas853@gmail.com>
2026-07-17 16:31:59 +0530

    Expand memory manager header with allocation APIs

--- core/memory.h
+++ core/memory.h
@@ -1,6 +1,24 @@
 #ifndef MEMORY_H
 #define MEMORY_H
 
+#include <stddef.h>
+#include <stdint.h>
+
+#define PAGE_SIZE 4096
+
+// Initialize memory manager
 void memory_init(void);
+
+// Allocate memory block
+void *memory_alloc(size_t size);
+
+// Free allocated memory
+void memory_free(void *ptr);
+
+// Get total memory available
+uint64_t memory_get_total(void);
+
+// Get used memory
+uint64_t memory_get_used(void);
 
 #endif

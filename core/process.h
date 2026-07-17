ba579e9ad46f3798e638c034be0dd52ad77150d0
guptatejas853-crypto <guptatejas853@gmail.com>
2026-07-17 16:38:00 +0530

    Add initial GlassOS process management interface

--- core/process.h
+++ core/process.h
@@ -1,6 +1,17 @@
 #ifndef GLASSOS_PROCESS_H
 #define GLASSOS_PROCESS_H
 
+#include <stdint.h>
+
+typedef struct {
+    uint32_t pid;
+    char name[32];
+    uint8_t state;
+} process_t;
+
 void process_init(void);
+int process_create(const char* name);
+void process_kill(uint32_t pid);
+void process_schedule(void);
 
 #endif

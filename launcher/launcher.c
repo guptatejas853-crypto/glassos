1c1041e192c7ed307e4c91854c16035cb50cb6fb
guptatejas853-crypto <guptatejas853@gmail.com>
2026-07-17 16:29:53 +0530

    Integrate Application Launcher with Core Manager

--- launcher/launcher.c
+++ launcher/launcher.c
@@ -1,21 +1,25 @@
 #include <stdio.h>
 
 #include "launcher.h"
+
 #include "../core/process.h"
+#include "../core/memory.h"
+#include "../core/scheduler.h"
 
 int launch_application(const char *path)
 {
     printf("[Launcher] Launching Application: %s\n", path);
 
-    /*
-     * Future:
-     * Load ATP executable
-     * Allocate process
-     * Allocate memory
-     * Start execution
-     */
+    /* Allocate memory */
+    memory_allocate();
 
+    /* Create process */
     process_create(path);
+
+    /* Add process to scheduler */
+    scheduler_add_process(path);
+
+    printf("[Launcher] Application Started Successfully.\n");
 
     return 1;
 }

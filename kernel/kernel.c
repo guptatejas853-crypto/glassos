ff391eaeec9cd2372f5ff4adc3f2c3fb24466d30
guptatejas853-crypto <guptatejas853@gmail.com>
2026-07-17 17:51:44 +0530

    Integrate Kernel with GlassOS Subsystems

--- kernel/kernel.c
+++ kernel/kernel.c
@@ -19,30 +19,20 @@ void kernel_start(void)
 
     printf("[Kernel] Starting subsystems...\n");
 
-    /* Memory Manager */
     memory_init();
     printf("[Kernel] Memory Manager Ready.\n");
 
-
-    /* Process Manager */
     process_init();
     printf("[Kernel] Process Manager Ready.\n");
 
-
-    /* Scheduler */
     scheduler_init();
     printf("[Kernel] Scheduler Ready.\n");
 
-
-    /* Virtual File System */
     vfs_init();
     printf("[Kernel] VFS Ready.\n");
 
-
-    /* Smart ATP Engine */
     smart_atp_init();
     printf("[Kernel] Smart ATP Engine Ready.\n");
-
 
     printf("[Kernel] GlassOS initialized successfully.\n");
 }

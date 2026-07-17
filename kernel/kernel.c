f838e0917894603a182f84a773241c79deca5037
guptatejas853-crypto <guptatejas853@gmail.com>
2026-07-17 19:20:08 +0530

    Initialize Glass Browser from Kernel

--- kernel/kernel.c
+++ kernel/kernel.c
@@ -10,6 +10,8 @@
 
 #include "../compatibility/smart_atp_engine.h"
 
+#include "../browser/browser.h"
+
 
 void kernel_start(void)
 {
@@ -17,22 +19,33 @@ void kernel_start(void)
     printf("       GlassOS Kernel v0.1       \n");
     printf("=================================\n");
 
+
     printf("[Kernel] Starting subsystems...\n");
+
 
     memory_init();
     printf("[Kernel] Memory Manager Ready.\n");
 
+
     process_init();
     printf("[Kernel] Process Manager Ready.\n");
+
 
     scheduler_init();
     printf("[Kernel] Scheduler Ready.\n");
 
+
     vfs_init();
     printf("[Kernel] VFS Ready.\n");
 
+
     smart_atp_init();
     printf("[Kernel] Smart ATP Engine Ready.\n");
+
+
+    browser_init();
+    printf("[Kernel] Glass Browser Ready.\n");
+
 
     printf("[Kernel] GlassOS initialized successfully.\n");
 }

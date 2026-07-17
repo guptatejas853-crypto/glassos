eb3cd4178bba6aa5664e5805bbb169ee02c7e249
guptatejas853-crypto <guptatejas853@gmail.com>
2026-07-17 14:55:40 +0530

    Add keyboard driver

--- drivers/keyboard.c
+++ drivers/keyboard.c
@@ -1,6 +1,7 @@
-#ifndef KEYBOARD_H
-#define KEYBOARD_H
+#include <stdio.h>
+#include "keyboard.h"
 
-void keyboard_init(void);
-
-#endif
+void keyboard_init(void)
+{
+    printf("Keyboard Driver Initialized\n");
+}

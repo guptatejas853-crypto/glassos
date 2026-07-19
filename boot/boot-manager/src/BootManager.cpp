49e470a4951d859738d0737e4b35f30335389db7
guptatejas853-crypto <guptatejas853@gmail.com>
2026-07-19 16:03:40 +0530

    feat(boot): implement BootManager boot flow

---
+++ boot/boot-manager/src/BootManager.cpp
@@ -0,0 +1,81 @@
+#include "BootManager.h"
+
+#include <iostream>
+
+using namespace GlassOS;
+
+BootManager::BootManager()
+{
+}
+
+bool BootManager::Initialize()
+{
+    std::cout << "[BootManager] Initializing...\n";
+    return true;
+}
+
+BootMode BootManager::DetectBootMode()
+{
+    if (WasLastShutdownUnexpected())
+        return BootMode::RecoverySession;
+
+    return BootMode::Normal;
+}
+
+void BootManager::Start()
+{
+    BootMode mode = DetectBootMode();
+
+    switch (mode)
+    {
+        case BootMode::Normal:
+            StartNormalBoot();
+            break;
+
+        case BootMode::RecoverySession:
+            StartRecoverySession();
+            break;
+
+        case BootMode::RecoveryCenter:
+            StartRecoveryCenter();
+            break;
+
+        case BootMode::SafeMode:
+            StartSafeMode();
+            break;
+
+        case BootMode::Diagnostics:
+            StartDiagnostics();
+            break;
+    }
+}
+
+bool BootManager::WasLastShutdownUnexpected()
+{
+    return false;
+}
+
+void BootManager::StartNormalBoot()
+{
+    std::cout << "[BootManager] Normal Boot\n";
+}
+
+void BootManager::StartRecoverySession()
+{
+    std::cout << "[BootManager] Recovery Session\n";
+}
+
+void BootManager::StartRecoveryCenter()
+{
+    std::cout << "[BootManager] Recovery Center\n";
+}
+
+void BootManager::StartSafeMode()
+{
+    std::cout << "[BootManager] Safe Mode\n";
+}
+
+void BootManager::StartDiagnostics()
+{
+    std::cout << "[BootManager] Diagnostics Mode\n";
+}

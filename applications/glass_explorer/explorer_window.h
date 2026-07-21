69552a3c8665bf2d44f46d0610ffd897fc970af9
guptatejas853-crypto <guptatejas853@gmail.com>
2026-07-21 15:37:58 +0530

    feat(explorer): define ExplorerWindow interface

---
+++ applications/glass_explorer/explorer_window.h
@@ -0,0 +1,20 @@
+#pragma once
+
+namespace GlassOS
+{
+    class ExplorerWindow
+    {
+    public:
+        static bool Initialize();
+
+        static bool Create();
+
+        static bool Show();
+
+        static bool Hide();
+
+        static bool Close();
+
+        static bool Refresh();
+    };
+}

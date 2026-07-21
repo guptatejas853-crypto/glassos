40832556e32b7501507b159e7beab2c4629d4923
guptatejas853-crypto <guptatejas853@gmail.com>
2026-07-21 16:47:14 +0530

    feat(settings): implement AccountManager

--- applications/glass_settings/account_manager.cpp
+++ applications/glass_settings/account_manager.cpp
@@ -1,1 +1,62 @@
+#include "account_manager.h"
 
+using namespace GlassOS;
+
+std::string AccountManager::username;
+
+bool AccountManager::accountExists = false;
+
+
+bool AccountManager::Initialize()
+{
+    username.clear();
+
+    accountExists = false;
+
+    return true;
+}
+
+
+bool AccountManager::CreateAccount(
+    const std::string& name)
+{
+    username = name;
+
+    accountExists = true;
+
+    return true;
+}
+
+
+bool AccountManager::SetUsername(
+    const std::string& name)
+{
+    if(!accountExists)
+        return false;
+
+    username = name;
+
+    return true;
+}
+
+
+std::string AccountManager::GetUsername()
+{
+    return username;
+}
+
+
+bool AccountManager::DeleteAccount()
+{
+    username.clear();
+
+    accountExists = false;
+
+    return true;
+}
+
+
+bool AccountManager::Exists()
+{
+    return accountExists;
+}

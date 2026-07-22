0029958dd89b0936831f32c32e10e421dc0fad9b
guptatejas853-crypto <guptatejas853@gmail.com>
2026-07-22 15:59:28 +0530

    feat(startmenu): add application launch shortcuts

--- desktop/start_menu.cpp
+++ desktop/start_menu.cpp
@@ -1,86 +1,30 @@
 #include "start_menu.h"
 
-#include <iostream>
+#include "../applications/application_launcher.h"
 
 using namespace GlassOS;
 
-bool StartMenu::visible = false;
-
 bool StartMenu::Initialize()
 {
-    visible = false;
     return true;
 }
 
-bool StartMenu::Show()
+void StartMenu::OpenExplorer()
 {
-    visible = true;
-
-    DrawHeader();
-    DrawPinnedApps();
-    DrawAllApps();
-    DrawPowerMenu();
-    DrawUserProfile();
-
-    std::cout << "[StartMenu] Opened\n";
-
-    return true;
+    ApplicationLauncher::Launch("Explorer");
 }
 
-bool StartMenu::Hide()
+void StartMenu::OpenSettings()
 {
-    visible = false;
-
-    std::cout << "[StartMenu] Closed\n";
-
-    return true;
+    ApplicationLauncher::Launch("Settings");
 }
 
-bool StartMenu::Toggle()
+void StartMenu::OpenNotebook()
 {
-    if (visible)
-        return Hide();
-
-    return Show();
+    ApplicationLauncher::Launch("Notebook");
 }
 
-bool StartMenu::Refresh()
+void StartMenu::OpenTerminal()
 {
-    if (!visible)
-        return true;
-
-    DrawPinnedApps();
-    DrawAllApps();
-
-    return true;
-}
-
-bool StartMenu::DrawHeader()
-{
-    std::cout << "[StartMenu] Header Loaded\n";
-    return true;
-}
-
-bool StartMenu::DrawPinnedApps()
-{
-    std::cout << "[StartMenu] Pinned Apps Loaded\n";
-    return true;
-}
-
-bool StartMenu::DrawAllApps()
-{
-    std::cout << "[StartMenu] All Apps Loaded\n";
-    return true;
-}
-
-bool StartMenu::DrawPowerMenu()
-{
-    std::cout << "[StartMenu] Power Menu Loaded\n";
-    return true;
-}
-
-bool StartMenu::DrawUserProfile()
-{
-    std::cout << "[StartMenu] User Profile Loaded\n";
-    return true;
+    ApplicationLauncher::Launch("Terminal");
 }

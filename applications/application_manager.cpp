37c4e58bb546b455b8bdf49c6995a328e217f8e0
guptatejas853-crypto <guptatejas853@gmail.com>
2026-07-22 15:47:56 +0530

    feat(app): implement ApplicationManager

---
+++ applications/application_manager.cpp
@@ -0,0 +1,27 @@
+#include "application_manager.h"
+
+using namespace GlassOS;
+
+std::vector<
+    std::shared_ptr<Application>>
+ApplicationManager::m_apps;
+
+bool ApplicationManager::Initialize()
+{
+    m_apps.clear();
+
+    return true;
+}
+
+void ApplicationManager::AddApplication(
+    std::shared_ptr<Application> app)
+{
+    m_apps.push_back(app);
+}
+
+const std::vector<
+    std::shared_ptr<Application>>&
+ApplicationManager::GetApplications()
+{
+    return m_apps;
+}

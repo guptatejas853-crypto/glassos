f95a35871699981295f763668efb55f5ea571ea9
guptatejas853-crypto <guptatejas853@gmail.com>
2026-07-22 16:08:43 +0530

    feat(explorer): define file search

--- applications/glass_explorer/search_engine.h
+++ applications/glass_explorer/search_engine.h
@@ -8,18 +8,10 @@ namespace GlassOS
     class SearchEngine
     {
     public:
-        static bool Initialize();
 
-        static std::vector<std::string> Search(
-            const std::string& directory,
-            const std::string& query);
-
-        static void ClearResults();
-
-        static const std::vector<std::string>&
-            GetResults();
-
-    private:
-        static std::vector<std::string> results;
+        static std::vector<std::string>
+        Search(
+            const std::string& path,
+            const std::string& keyword);
     };
 }

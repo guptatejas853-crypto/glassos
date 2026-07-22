9f5a9b034030883eb52358bb9f05aaa051ba921d
guptatejas853-crypto <guptatejas853@gmail.com>
2026-07-22 16:02:35 +0530

    feat(quickcenter): implement basic visibility

--- desktop/quick_center.cpp
+++ desktop/quick_center.cpp
@@ -1,7 +1,5 @@
 #include "quick_center.h"
 
-#include <iostream>
-
 using namespace GlassOS;
 
 bool QuickCenter::visible = false;
@@ -9,64 +7,21 @@ bool QuickCenter::visible = false;
 bool QuickCenter::Initialize()
 {
     visible = false;
+
     return true;
 }
 
-bool QuickCenter::Show()
+void QuickCenter::Show()
 {
     visible = true;
-
-    DrawHeader();
-    DrawTiles();
-    DrawBatteryCard();
-
-    std::cout << "[QuickCenter] Opened\n";
-
-    return true;
 }
 
-bool QuickCenter::Hide()
+void QuickCenter::Hide()
 {
     visible = false;
-
-    std::cout << "[QuickCenter] Closed\n";
-
-    return true;
 }
 
-bool QuickCenter::Toggle()
+bool QuickCenter::IsVisible()
 {
-    if (visible)
-        return Hide();
-
-    return Show();
-}
-
-bool QuickCenter::Refresh()
-{
-    if (!visible)
-        return true;
-
-    DrawTiles();
-    DrawBatteryCard();
-
-    return true;
-}
-
-bool QuickCenter::DrawHeader()
-{
-    std::cout << "[QuickCenter] Header Loaded\n";
-    return true;
-}
-
-bool QuickCenter::DrawTiles()
-{
-    std::cout << "[QuickCenter] Tiles Loaded\n";
-    return true;
-}
-
-bool QuickCenter::DrawBatteryCard()
-{
-    std::cout << "[QuickCenter] Battery Card Loaded\n";
-    return true;
+    return visible;
 }

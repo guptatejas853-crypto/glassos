b4d169d11a1b550b7bf235cd6d8001bf45a973ca
guptatejas853-crypto <guptatejas853@gmail.com>
2026-07-22 16:17:43 +0530

    feat(explorer): implement basic recycle bin

--- applications/glass_explorer/recycle_bin.h
+++ applications/glass_explorer/recycle_bin.h
@@ -5,30 +5,24 @@
 
 namespace GlassOS
 {
-    struct RecycleItem
-    {
-        std::string originalPath;
-        std::string recyclePath;
-        std::string deletedTime;
-    };
-
     class RecycleBin
     {
     public:
+
         static bool Initialize();
 
-        static bool Delete(
+        static void Move(
             const std::string& path);
 
-        static bool Restore(
-            const std::string& recyclePath);
+        static void Restore(
+            const std::string& path);
 
-        static bool Empty();
+        static void Empty();
 
-        static const std::vector<RecycleItem>&
-            GetItems();
+        static std::vector<std::string> Items();
 
     private:
-        static std::vector<RecycleItem> items;
+
+        static std::vector<std::string> files;
     };
 }

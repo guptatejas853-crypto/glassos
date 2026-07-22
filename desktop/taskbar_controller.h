ee3ea32bc9ad4a8ba608fef0cebfc1db4fcee0ac
guptatejas853-crypto <guptatejas853@gmail.com>
2026-07-22 15:56:23 +0530

    feat(taskbar): define TaskbarController

---
+++ desktop/taskbar_controller.h
@@ -0,0 +1,19 @@
+#pragma once
+
+namespace GlassOS
+{
+    class TaskbarController
+    {
+    public:
+
+        static bool Initialize();
+
+        static void OpenExplorer();
+
+        static void OpenSettings();
+
+        static void OpenNotebook();
+
+        static void OpenTerminal();
+    };
+}

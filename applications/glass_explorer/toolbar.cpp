b39710e9d8d98cf18d142d27ad7752930b3cd71f
guptatejas853-crypto <guptatejas853@gmail.com>
2026-07-22 16:15:32 +0530

    feat(explorer): implement basic toolbar controls

--- applications/glass_explorer/toolbar.cpp
+++ applications/glass_explorer/toolbar.cpp
@@ -2,37 +2,23 @@
 
 using namespace GlassOS;
 
-std::vector<ToolbarButton> Toolbar::buttons;
+bool Toolbar::ready = false;
 
 bool Toolbar::Initialize()
 {
-    return LoadDefaultButtons();
-}
-
-bool Toolbar::LoadDefaultButtons()
-{
-    buttons.clear();
-
-    buttons.push_back({"back", "Back"});
-    buttons.push_back({"forward", "Forward"});
-    buttons.push_back({"up", "Up"});
-    buttons.push_back({"refresh", "Refresh"});
-
-    buttons.push_back({"newfolder", "New Folder"});
-
-    buttons.push_back({"copy", "Copy"});
-    buttons.push_back({"cut", "Cut"});
-    buttons.push_back({"paste", "Paste"});
-
-    buttons.push_back({"delete", "Delete"});
-
-    buttons.push_back({"search", "Search"});
+    ready = true;
 
     return true;
 }
 
-const std::vector<ToolbarButton>&
-Toolbar::GetButtons()
+void Toolbar::Back()
 {
-    return buttons;
+}
+
+void Toolbar::Forward()
+{
+}
+
+void Toolbar::Refresh()
+{
 }

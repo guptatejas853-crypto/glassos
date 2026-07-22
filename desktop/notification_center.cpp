bbd6b897db5472ef64bcbbfa8759e8dcbd3a12bd
guptatejas853-crypto <guptatejas853@gmail.com>
2026-07-22 16:04:08 +0530

    feat(notification): implement basic notification center

--- desktop/notification_center.cpp
+++ desktop/notification_center.cpp
@@ -1,7 +1,5 @@
 #include "notification_center.h"
 
-#include <iostream>
-
 using namespace GlassOS;
 
 bool NotificationCenter::visible = false;
@@ -9,77 +7,21 @@ bool NotificationCenter::visible = false;
 bool NotificationCenter::Initialize()
 {
     visible = false;
+
     return true;
 }
 
-bool NotificationCenter::Show()
+void NotificationCenter::Show()
 {
     visible = true;
-
-    DrawHeader();
-    DrawNotificationList();
-    DrawFooter();
-
-    std::cout << "[NotificationCenter] Opened\n";
-
-    return true;
 }
 
-bool NotificationCenter::Hide()
+void NotificationCenter::Hide()
 {
     visible = false;
-
-    std::cout << "[NotificationCenter] Closed\n";
-
-    return true;
 }
 
-bool NotificationCenter::Toggle()
+bool NotificationCenter::IsVisible()
 {
-    if (visible)
-        return Hide();
-
-    return Show();
-}
-
-bool NotificationCenter::AddNotification(
-    const std::string& title,
-    const std::string& message)
-{
-    std::cout
-        << "[Notification] "
-        << title
-        << " : "
-        << message
-        << std::endl;
-
-    return true;
-}
-
-bool NotificationCenter::ClearAll()
-{
-    std::cout << "[NotificationCenter] Cleared\n";
-
-    return true;
-}
-
-bool NotificationCenter::DrawHeader()
-{
-    std::cout << "[NotificationCenter] Header Loaded\n";
-
-    return true;
-}
-
-bool NotificationCenter::DrawNotificationList()
-{
-    std::cout << "[NotificationCenter] Notification List Loaded\n";
-
-    return true;
-}
-
-bool NotificationCenter::DrawFooter()
-{
-    std::cout << "[NotificationCenter] Footer Loaded\n";
-
-    return true;
+    return visible;
 }

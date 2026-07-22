1243ed796043f3e335128cd5a36dc2b55b1c68e5
guptatejas853-crypto <guptatejas853@gmail.com>
2026-07-22 16:10:44 +0530

    feat(explorer): implement basic address bar navigation

--- applications/glass_explorer/address_bar.cpp
+++ applications/glass_explorer/address_bar.cpp
@@ -2,7 +2,7 @@
 
 using namespace GlassOS;
 
-std::string AddressBar::currentPath;
+std::string AddressBar::currentPath = "/";
 
 bool AddressBar::Initialize()
 {
@@ -11,30 +11,13 @@ bool AddressBar::Initialize()
     return true;
 }
 
-bool AddressBar::SetPath(
+void AddressBar::SetPath(
     const std::string& path)
 {
     currentPath = path;
-
-    return true;
 }
 
 std::string AddressBar::GetPath()
 {
     return currentPath;
-}
-
-bool AddressBar::Navigate(
-    const std::string& path)
-{
-    currentPath = path;
-
-    return true;
-}
-
-bool AddressBar::Clear()
-{
-    currentPath.clear();
-
-    return true;
 }

21117a880e1179e1fb4bdfb4e7c85ae37b0119d9
guptatejas853-crypto <guptatejas853@gmail.com>
2026-07-23 15:42:31 +0530

    feat(settings): implement display manager

--- applications/glass_settings/display_manager.h
+++ applications/glass_settings/display_manager.h
@@ -7,35 +7,17 @@ namespace GlassOS
     class DisplayManager
     {
     public:
+
         static bool Initialize();
 
-        static bool SetResolution(
+        static void SetResolution(
             const std::string& resolution);
 
         static std::string GetResolution();
 
-        static bool SetRefreshRate(
-            int rate);
+        static void SetBrightness(
+            int brightness);
 
-        static int GetRefreshRate();
-
-        static bool SetScaling(
-            int scale);
-
-        static int GetScaling();
-
-        static bool SetOrientation(
-            const std::string& orientation);
-
-        static std::string GetOrientation();
-
-    private:
-        static std::string resolution;
-
-        static int refreshRate;
-
-        static int scaling;
-
-        static std::string orientation;
+        static int GetBrightness();
     };
 }

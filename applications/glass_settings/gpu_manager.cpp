51dd912c0f227842fb68c88eb5dc13a7c913ed63
guptatejas853-crypto <guptatejas853@gmail.com>
2026-07-23 15:53:26 +0530

    feat(gpu): implement basic gpu manager

--- applications/glass_settings/gpu_manager.cpp
+++ applications/glass_settings/gpu_manager.cpp
@@ -2,125 +2,17 @@
 
 using namespace GlassOS;
 
-
-std::string GPUManager::gpuType =
-    "Integrated";
-
-
-std::string GPUManager::gpuName =
-    "Generic Graphics";
-
-
-std::string GPUManager::renderingMode =
-    "Auto";
-
-
-bool GPUManager::acceleration = true;
-
-
-
 bool GPUManager::Initialize()
 {
-    /*
-        Future hardware detection:
-
-        Integrated:
-        - Intel UHD
-        - AMD Radeon iGPU
-
-        Dedicated:
-        - NVIDIA RTX
-        - AMD Radeon RX
-
-        Hybrid:
-        - Integrated + Dedicated
-    */
-
-    // TODO:
-// Detect GPU from GlassOS HAL / operating system.
-
-gpuType = "Unknown";
-gpuName = "Unknown GPU";
-
-
-    renderingMode =
-        "Auto";
-
-
-    acceleration = true;
-
-
     return true;
 }
 
-
-
-std::string GPUManager::GetGPUType()
+std::string GPUManager::GPUName()
 {
-    return gpuType;
+    return "Unknown GPU";
 }
 
-
-
-std::string GPUManager::GetGPUName()
+bool GPUManager::DedicatedGPU()
 {
-    return gpuName;
-}
-
-
-
-bool GPUManager::HasDedicatedGPU()
-{
-    return gpuType == "Dedicated"
-        || gpuType == "Hybrid";
-}
-
-
-
-bool GPUManager::HasIntegratedGPU()
-{
-    return gpuType == "Integrated"
-        || gpuType == "Hybrid";
-}
-
-
-
-bool GPUManager::EnableAcceleration()
-{
-    acceleration = true;
-
-    return true;
-}
-
-
-
-bool GPUManager::DisableAcceleration()
-{
-    acceleration = false;
-
-    return true;
-}
-
-
-
-bool GPUManager::IsAccelerationEnabled()
-{
-    return acceleration;
-}
-
-
-
-bool GPUManager::SetRenderingMode(
-    const std::string& mode)
-{
-    renderingMode = mode;
-
-    return true;
-}
-
-
-
-std::string GPUManager::GetRenderingMode()
-{
-    return renderingMode;
+    return false;
 }

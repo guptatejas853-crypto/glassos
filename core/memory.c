04ae2e8d4fff935a22411daad57956ad25b3dc96
guptatejas853-crypto <guptatejas853@gmail.com>
2026-07-25 16:18:20 +0530

    feat(core): implement basic memory manager

--- core/memory.c
+++ core/memory.c
@@ -1,7 +1,34 @@
 #include <stdio.h>
+
 #include "memory.h"
+
+#define MEMORY_POOL_SIZE (1024 * 1024)
+
+static unsigned char memory_pool[MEMORY_POOL_SIZE];
+static unsigned int memory_offset = 0;
 
 void memory_init(void)
 {
-    printf("Memory Manager Initialized\n");
+    memory_offset = 0;
+
+    printf("[Memory] Memory Manager Initialized.\n");
+}
+
+void* memory_alloc(unsigned int size)
+{
+    if (memory_offset + size > MEMORY_POOL_SIZE)
+        return NULL;
+
+    void* ptr = &memory_pool[memory_offset];
+
+    memory_offset += size;
+
+    return ptr;
+}
+
+void memory_free(void* ptr)
+{
+    (void)ptr;
+
+    printf("[Memory] Free Requested\n");
 }

3374834bc9ae9f475384c4b12879db1ca966b155
guptatejas853-crypto <guptatejas853@gmail.com>
2026-07-25 15:26:41 +0530

    feat(desktop): implement basic WindowManager

--- desktop/window_manager.cpp
+++ desktop/window_manager.cpp
@@ -1,28 +1,63 @@
 #include "window_manager.h"
 
+#include <vector>
+#include <algorithm>
+
 using namespace GlassOS;
+
+namespace
+{
+    std::vector<int> windows;
+    bool running = false;
+}
 
 bool WindowManager::Initialize()
 {
+    windows.clear();
+    running = false;
     return true;
 }
 
 bool WindowManager::Start()
 {
+    running = true;
     return true;
 }
 
 bool WindowManager::Refresh()
 {
-    return true;
+    return running;
 }
 
 bool WindowManager::Lock()
 {
+    running = false;
     return true;
 }
 
 bool WindowManager::Shutdown()
 {
+    windows.clear();
+    running = false;
     return true;
+}
+
+bool WindowManager::RegisterWindow(int id)
+{
+    windows.push_back(id);
+    return true;
+}
+
+bool WindowManager::CloseWindow(int id)
+{
+    windows.erase(
+        std::remove(windows.begin(), windows.end(), id),
+        windows.end());
+
+    return true;
+}
+
+int WindowManager::WindowCount()
+{
+    return static_cast<int>(windows.size());
 }

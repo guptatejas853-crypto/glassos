c9154c4b428f2aa25d70b1763daf54642a8835fa
guptatejas853-crypto <guptatejas853@gmail.com>
2026-07-25 12:41:06 +0530

    feat(desktop): add basic WindowManager integration

---
+++ desktop/window_manager.cpp
@@ -0,0 +1,28 @@
+#include "window_manager.h"
+
+using namespace GlassOS;
+
+bool WindowManager::Initialize()
+{
+    return true;
+}
+
+bool WindowManager::Start()
+{
+    return true;
+}
+
+bool WindowManager::Refresh()
+{
+    return true;
+}
+
+bool WindowManager::Lock()
+{
+    return true;
+}
+
+bool WindowManager::Shutdown()
+{
+    return true;
+}

dc30274a382842ef2ab8511a23357c501d7a39bc
guptatejas853-crypto <guptatejas853@gmail.com>
2026-07-25 15:27:20 +0530

    feat(desktop): implement basic WindowManager

--- desktop/window_manager.h
+++ desktop/window_manager.h
@@ -7,10 +7,20 @@ class WindowManager
 {
 public:
     static bool Initialize();
+
     static bool Start();
+
     static bool Refresh();
+
     static bool Lock();
+
     static bool Shutdown();
+
+    static bool RegisterWindow(int id);
+
+    static bool CloseWindow(int id);
+
+    static int WindowCount();
 };
 
 }

166892b57c045337319ab132a6241e3973468a5a
guptatejas853-crypto <guptatejas853@gmail.com>
2026-07-25 16:18:50 +0530

    feat(core): implement basic memory manager

--- core/memory.h
+++ core/memory.h
@@ -1,24 +1,10 @@
-#ifndef MEMORY_H
-#define MEMORY_H
+#ifndef GLASSOS_MEMORY_H
+#define GLASSOS_MEMORY_H
 
-#include <stddef.h>
-#include <stdint.h>
-
-#define PAGE_SIZE 4096
-
-// Initialize memory manager
 void memory_init(void);
 
-// Allocate memory block
-void *memory_alloc(size_t size);
+void* memory_alloc(unsigned int size);
 
-// Free allocated memory
-void memory_free(void *ptr);
-
-// Get total memory available
-uint64_t memory_get_total(void);
-
-// Get used memory
-uint64_t memory_get_used(void);
+void memory_free(void* ptr);
 
 #endif
